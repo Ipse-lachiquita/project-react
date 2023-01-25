913af74e68d1faf61deaf305fe31e5b1e9025f61
*lBRabZm8uf <is.lee@rsad.co.kr>
2023-01-25 11:00:53 +0900

    done
    done

--- practice-react/src/pages/Login.js
+++ practice-react/src/pages/Login.js
@@ -1,31 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import styled from 'styled-components'
 import HeaderLogin from '../molecules/HeaderLogin'
 import StatuesBar from '../atoms/StatuesBar'
 import InputLogin from '../atoms/InputLogin'
 import FindAccount from '../atoms/FindAccount'
 import ButtonLink from '../molecules/ButtonLink'
+import Calculator from 'awesome-react-calculator'
+import './styles.css'
+
+function onResultChange(result) {
+  console.log(result)
+}
 
 function Login() {
+  // return (
+  //   <>
+  //     <HeaderLogin
+  //       title="Welcome to Kokoa Clone"
+  //       text="If you have a Kokoa Account, log in with your email or phone number."
+  //     />
+  //     <Form>
+  //       <InputLogin
+  //         name="username"
+  //         type="text"
+  //         placeholder="Email or phone number"
+  //       />
+  //       <InputLogin name="password" type="password" placeholder="Password" />
+  //       <ButtonLink to="/Friends" />
+  //       <FindAccount text="Find Kokoa Account or Password" />
+  //     </Form>
+  //   </>
+  // )
   return (
-    <>
-      <HeaderLogin
-        title="Welcome to Kokoa Clone"
-        text="If you have a Kokoa Account, log in with your email or phone number."
-      />
-      <Form>
-        <InputLogin
-          name="username"
-          type="text"
-          placeholder="Email or phone number"
-        />
-        <InputLogin name="password" type="password" placeholder="Password" />
-        <ButtonLink to="/Friends" />
-        <FindAccount text="Find Kokoa Account or Password" />
-      </Form>
-    </>
+    <div className="calculator" style={{ margin: 30 }}>
+      <Calculator />
+    </div>
   )
 }
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<Login />, rootElement)
 
 const Form = styled.form`
   display: flex;
--- practice-react/src/pages/More.js
+++ practice-react/src/pages/More.js
@@ -1,8 +1,28 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper'
+import React from 'react'
+
+import 'swiper/swiper.scss'
+import 'swiper/components/navigation/navigation.scss'
+import 'swiper/components/pagination/pagination.scss'
+
 function More() {
+  SwiperCore.use([Navigation, Pagination, Autoplay])
   return (
-    <>
-      <h1>More</h1>
-    </>
+    <Swiper
+      style={{ height: '90px' }}
+      spaceBetween={50}
+      slidesPerView={2}
+      onSlideChange={() => console.log('slide change')}
+      pagination={{ clickable: true }}
+      loop={true}
+      autoplay={true}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
   )
 }
 
